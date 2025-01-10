6c113868330f1708b141262e3e906351d1bedfb0
Naveen Kumar <naveenkk201113@gmail.com>
2025-01-10 11:32:52 +0530

    created flask API and used stremlit as frontend

---
+++ Titanic_dep.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import streamlit as st
+import requests
+
+st.title('Titanic passengers survival prediction')
+
+st.write('This is a simple example of a Machine Learning model deployment using Streamlit.')
+
+
+user_input_pclass = st.selectbox('Pclass', [1, 2, 3])
+user_input_sex = st.selectbox('Sex', ['Male', 'Female'])
+user_input_age = st.number_input('Age', min_value=0, max_value=100, value=30)
+
+
+if st.button('Predict'):
+    #send input data to Flask API
+    response = requests.post('http://localhost:5000/predict', json={'Pclass': user_input_pclass, 'Sex': user_input_sex, 'Age': user_input_age})
+                                                                    
+    if response.status_code == 200:
+        st.write('Prediction:', response.text)
+    else:
+        st.write('Error:', response.text)
+        
+
+else:
+    st.write('Please input values to predict survival')
--- app.py
+++ app.py
@@ -1,11 +1,47 @@
-from flask import Flask
+from flask import Flask, request
 import pickle
 
-app = Flask(__name__) # create an app instance
+first_app = Flask(__name__) # create an app instance
 
+with open("rfclf.pkl", "rb") as f:
+    model = pickle.load(f)
 
-
-@app.route("/ping", methods=["GET"])
+@first_app.route("/ping", methods=["GET"])
 def ping():
     return "pong"
 
+@first_app.route("/home", methods=["GET"])
+def home_ping():
+    return "home-pong"
+
+@first_app.route("/predict", methods=["POST"])
+def predict():
+    input_params = request.get_json()
+    Pclass = input_params['Pclass']
+
+    if Pclass not in [1,2,3]:
+        return "Invalid data input in Pclass"
+    
+    if input_params['Sex'] == 'Male':
+        Sex = 0
+    elif input_params['Sex'] == 'Female':
+        Sex = 1
+    else:
+        return "Invalid data input in Sex"
+    
+    Age = input_params['Age']
+
+    data = [[Pclass, Sex, Age, 1,0,0,1,0]]
+    result = model.predict(data)
+
+    if result == 1:
+        return "Survived"
+    else:
+        return "Not survived"
+    
+
+
+
+
+
+first_app.run()
